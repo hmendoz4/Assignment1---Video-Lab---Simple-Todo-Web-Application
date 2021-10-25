38ec2100a68a2a551e1b7a4fb2bc1159ee9aa07a
hmendoz4 <henrymendoza17542015@gmail.com>
2021-10-24 20:53:35 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
+//DRY Version, Don't Repeat yourself
 const todos = [];
 
-const pendingClasses =
-  "bg-white w-full text-center text-blue-500 rounded py-4 border-2 border-blue-500 transition transform ease-in-out duration-300 hover:bg-blue-500 hover:text-white hover:scale-110 hover:rotate-1 cursor-pointer";
-const completedClasses =
-  "bg-white w-full text-center text-red-500 rounded py-4 border-2 border-red-500 transition transform ease-in-out duration-300 hover:bg-red-500 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer";
+const get = (elements) => 
+elements.map((element) => document.getElementById
+(element));
 
-const pendingList = document.getElementById("pendingList");
-const completedList = document.getElementById("completedList");
+const [pendingList, completedList, addForm, newTodo] = get([
+  "pendingList", 
+  "completedList", 
+  "addForm", 
+  "newTodo"
+]);
+
+const newList = [
+  {
+    element: pendingList,
+    status: "pending",
+  },
+  {
+    element: completedList,
+    status: "done",
+  },
+];
+
+const cssClasses = {
+  pending:   "bg-white w-full text-center text-blue-500 rounded py-4 border-2 border-blue-500 transition transform ease-in-out duration-300 hover:bg-blue-500 hover:text-white hover:scale-110 hover:rotate-1 cursor-pointer",
+  done:  "bg-white w-full text-center text-red-500 rounded py-4 border-2 border-red-500 transition transform ease-in-out duration-300 hover:bg-red-500 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer",
+
+};
+
+const updateTodos = () => {
+  newList.forEach((list) => {
+    const filteredTodos = todos.filter((todo) => todo.status === list.status);
+
+    list.element.innerHTML = ""
+    filteredTodos.forEach((todo) => {
+      const item = document.createElement("li");
+      item.className = cssClasses[list.status];
+      item.innerText = todo.text;
+      item.id = todo.id;
+      list.element.appendChild(item);
+    });
+  });
+};
+
 
 const showTodos = () => {
   const pendingTodos = todos.filter((todo) => todo.status === "pending");
@@ -14,7 +51,7 @@ const showTodos = () => {
   pendingList.innerHTML = "";
   pendingTodos.forEach((todo) => {
     const pendingItem = document.createElement("li");
-    pendingItem.className = pendingClasses;
+    pendingItem.className = cssClasses.pending;
     pendingItem.innerText = todo.text;
     pendingItem.id = todo.id;
     pendingList.appendChild(pendingItem);
@@ -25,15 +62,14 @@ const showTodos = () => {
   completedList.innerHTML = "";
   completedTodos.forEach((todo) => {
     const completedItem = document.createElement("li");
-    completedItem.className = completedClasses;
+    completedItem.className = cssClasses.done;
     completedItem.innerText = todo.text;
     completedItem.id = todo.id;
     completedList.appendChild(completedItem);
   });
 };
 
-const addForm = document.getElementById("addForm");
-const newTodo = document.getElementById("newTodo");
+
 
 addForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -43,15 +79,15 @@ addForm.addEventListener("submit", (event) => {
     status: "pending",
   });
   newTodo.value = "";
-  showTodos();
+  updateTodos();
 });
 
 pendingList.addEventListener("click", (event) => {
   todos.find((todo) => todo.id === event.target.id).status = "done";
-  showTodos();
+  updateTodos();
 });
 
 completedList.addEventListener("click", (event) => {
   todos.find((todo) => todo.id === event.target.id).status = "pending";
-  showTodos();
+  updateTodos();
 });
